2fadaf150b571ef591a4540ed8ada9f6a5aebe22
tishka25 <teodorstanishev@gmail.com>
2022-02-02 17:23:46 +0200

    add SearchFieldComponent

---
+++ src/components/SearchField/SearchField.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './style.css';
+import icon from './icons8-search.svg';
+
+export interface SearchFieldProps {
+    width?: string | number;
+    value?: string;
+    placeholder: string;
+    onChange?: (value: string) => string;
+    onFinishEdit?: (value: string) => string;
+}
+function SearchField(props: SearchFieldProps) {
+	const [currentValue, setCurrentValue] = useState(props.value || '');
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const value = e.target.value;
+		props.onChange && props.onChange(value);
+		setCurrentValue(value);
+	}
+
+	function handleSubmit() {
+		props.onFinishEdit && props.onFinishEdit(currentValue);
+	}
+
+	return (
+		<div className="search-container">
+			<div className="search">
+				<input type="text" className="searchTerm" placeholder={props.placeholder} onChange={handleChange}/>
+				<button type="submit" className="searchButton" onClick={handleSubmit}>
+					<img src={icon} />
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default SearchField;
