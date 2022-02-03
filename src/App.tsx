5bc7507abf5ab34381aac4dff0021cc7348c40d6
tishka25 <teodorstanishev@gmail.com>
2022-02-03 11:45:47 +0200

    remove unnesesary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { Skeleton } from 'antd';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import './App.css';
 import EmployeeTable from './components/EmployeeTable/EmployeeTable';
 import PageHeader from './components/PageHeader/PageHeader';
-import { RootReducers } from './redux';
-import { setEmployeeList } from './redux/actions/employeeListAction';
-import rewardGatewayService from './service/rewardGatewayService';
 
 function App () {
 
